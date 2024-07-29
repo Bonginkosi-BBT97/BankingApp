1556fe9ea2f067bd0d65b72eb1a43ace767a6faa
Bonginkosi-BBT <64373437+Bonginkosi-BBT97@users.noreply.github.com>
2024-07-29 15:24:19 +0200

    updated the viewmodel so that only the needed function is exposed to the view

--- Core/ViewModel/BankingViewModel.dart
+++ Core/ViewModel/BankingViewModel.dart
@@ -8,66 +8,65 @@ import '../ViewModel/BankingViewModel.dart';
 import 'dart:io';
 
 class ViewModel {
- Map<String, String> account = {};
-late String accountName;
-late String accountNumber; 
-late String amount;
+ Map<String, String> _account = {};
+late String _accountName;
+late String _accountNumber; 
+late String _amount;
 
 ViewModel(Map<String, String> account){
-  this.account = account;
-  this.accountName = account['BankAccountName'] ?? 'Unknown';
-  this.accountNumber = account['AccountNo'] ?? 'Unknown';
-  this.amount = account['Balance'] ?? '0.0';
+  this._account = account;
+  this._accountName = account['BankAccountName'] ?? 'Unknown';
+  this._accountNumber = account['AccountNo'] ?? 'Unknown';
+  this._amount = account['Balance'] ?? '0.0';
 }
  
   void showAccountDetails() {
-    print("Account Name: $accountName");
-    print("Account No: $accountNumber");
-    print("Acc Balance: R $amount");
-    handleUserInput();
+    print("Account Name: $_accountName");
+    print("Account No: $_accountNumber");
+    print("Acc Balance: R $_amount");
+    _handleUserInput();
   }
 
-   void desposit( String depositAmount) {
-      double current = double.parse(amount);
+   void _desposit( String depositAmount) {
+      double current = double.parse(_amount);
       double deposit = double.parse(depositAmount);
       double newAmount = current + deposit;
 
       print("R $depositAmount has been deposited");
-      amount = newAmount.toString();
+      _amount = newAmount.toString();
    }
 
-   void withdraw( String withdrawAmount) {
-     double current = double.parse(amount);
+   void _withdraw( String withdrawAmount) {
+     double current = double.parse(_amount);
       double withdraw = double.parse(withdrawAmount);
       double newAmount = current - withdraw;
 
       print("R $withdrawAmount has been withdrawn");
-      amount = newAmount.toString();
+      _amount = newAmount.toString();
    }
-  void instructions() {
+  void _instructions() {
     print("Enter 1 to withdraw");
     print("Enter 2 to deposit");
-    print("Enter 3 to view account details");
     print("Enter 0 to exit");
   }
 
-  void handleUserInput() {
+  void _handleUserInput() {
      while (true) {
-      instructions();
+      _instructions();
       String? input = stdin.readLineSync(); 
       switch (input) {
         case '1':
           print('Enter amount to withdraw:');
           String? withdrawAmount = stdin.readLineSync();
           if (withdrawAmount != null) {
-            withdraw(withdrawAmount);
+            _withdraw(withdrawAmount);
           }
           break;
         case '2':
           print('Enter amount to deposit:');
           String? depositAmount = stdin.readLineSync();
           if (depositAmount != null) {
-           desposit(depositAmount);
+           _desposit(depositAmount);
           }
           break;
           case '0':
@@ -75,7 +74,7 @@ ViewModel(Map<String, String> account){
           main();
           return;
         default:
-          print('Invalid input, please enter 1, 2, 3, or 0.');
+          print('Invalid input, please enter 1, 2, or 0.');
       }
       showAccountDetails();
    }
